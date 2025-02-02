c7eae98d5925c10cdcf78d72d1b32d6ce5c63569
joskapista01 <63414770+joskapista01@users.noreply.github.com>
2025-02-02 12:53:43 +0000

    fix

--- scheduler/githubrepo.go
+++ scheduler/githubrepo.go
@@ -83,10 +83,17 @@ func getGitHubClient(ctx context.Context, domainName string) *github.Client {
 	tc := oauth2.NewClient(ctx, ts)
 
 	// Determine API base URL
-	client := github.NewClient(tc)
-	if domainName != "github.com" {
-		client := github.NewEnterpriseClient(domainName, domainName, tc)
+	client = nil
+	if domainName == "github.com" {
+		client := github.NewClient(tc)
 	}
+	else {
+		client, err := github.NewEnterpriseClient(domainName, domainName, tc)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return client
 }
 
